430c9e7eb8970dd919919cfc3911b5bf52bf5e9a
Adam Smith <smith.adam937@gmail.com>
2022-05-20 11:05:18 -0500

    restructured if statements successfuly

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,9 @@ var specialCharacters = ["!", "#", "$", "%", "&", "'", "*", "(", ")"];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -18,15 +21,12 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 function generatePassword() {
   chosenChars = []; 
   randomPW = []; 
 
   var pwLength = parseInt(prompt("Enter a password length between 8 and 128 characters:"));
-
+debugger; 
   if (pwLength < 8 || pwLength > 128 || !pwLength) {
     alert("Oops! Try again"); 
       generatePassword(); 
@@ -34,29 +34,30 @@ function generatePassword() {
 
     
     var lowerPW = confirm("Click OK to INCLUDE LowerCase Characters in your Password. Click Cancel to Exclude.");
+      if(lowerPW) {
+      chosenChars = chosenChars.concat(lowerCharacters); 
+      }
+
     var upperPW = confirm("Click OK to INCLUDE UpperCase Characters in your Password. Click Cancel to Exclude."); 
+      if(upperPW) {
+      chosenChars = chosenChars.concat(upperCharacters);
+      }
+
     var numericPW = confirm("Click OK to INCLUDE Numeric Characters in your Password. Click Cancel to Exclude."); 
+      if(numericPW){
+      chosenChars = chosenChars.concat(numericCharacters);
+      }
+
     var specialPW = confirm("Click OK to INCLUDE Special Characters in your Password. Click Cancel to Exclude."); 
-      
+      if(specialPW){
+      chosenChars = chosenChars.concat(specialCharacters);
+      }
+
   if (!lowerPW && !upperPW && !numericPW && !specialPW) {
     alert("You have selected have the world's worst password of nothing!");
       generatePassword(); 
   }
 
-  if(lowerPW) {
-    chosenChars = chosenChars.concat(lowerCharacters); 
-  }
-  if(upperPW) {
-    chosenChars = chosenChars.concat(upperCharacters);
-  }
-  if(numericPW){
-    chosenChars = chosenChars.concat(numericCharacters);
-  }
-  if(specialPW){
-    chosenChars = chosenChars.concat(specialCharacters);
-  }
-
-console.log(chosenChars); 
 
 for (var i = 0; i < pwLength; i++) {
   randomPW[i] = chosenChars[Math.floor(Math.random() * chosenChars.length)];  
