c0b8d1c6c349d397f11d051c18a5607cfbba2dee
Adam Smith <smith.adam937@gmail.com>
2022-05-20 11:19:05 -0500

    descriptive comments added throughout script.js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
-//Create variables for different types of arrays 
-
+//Create arrays for different types of criteria that the user can select from 
 var lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; 
@@ -12,27 +11,31 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
+// Write password to the password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+// Display the password value and also remove the commas by utilizing the .join method 
   passwordText.value = password.join("");
 
 }
 
 function generatePassword() {
+  //create empty arrays for chosen criteria characterset and random password
   chosenChars = []; 
   randomPW = []; 
 
+  // Store the chosen length of the password into pwLength and convert into Number Type 
   var pwLength = parseInt(prompt("Enter a password length between 8 and 128 characters:"));
-debugger; 
+
+ // Check that the length of the password is between 8 - 128 . 
   if (pwLength < 8 || pwLength > 128 || !pwLength) {
     alert("Oops! Try again"); 
       generatePassword(); 
   } 
 
-    
+  //Begin criteria selection of LowerCase, UpperCase, Numeric, & special characters. If selected, store values in chosenChars array
     var lowerPW = confirm("Click OK to INCLUDE LowerCase Characters in your Password. Click Cancel to Exclude.");
       if(lowerPW) {
       chosenChars = chosenChars.concat(lowerCharacters); 
@@ -53,15 +56,17 @@ debugger;
       chosenChars = chosenChars.concat(specialCharacters);
       }
 
+    //If the user selects none of the above criteria, start over 
   if (!lowerPW && !upperPW && !numericPW && !specialPW) {
-    alert("You have selected have the world's worst password of nothing!");
+    alert("You have selected have the world's worst password of nothing! Please Try Again!");
       generatePassword(); 
   }
 
-
+  // Loop through the chosenChars Array selecting random characters using the Math.random method and store in randomPW array at respective index
 for (var i = 0; i < pwLength; i++) {
   randomPW[i] = chosenChars[Math.floor(Math.random() * chosenChars.length)];  
 }
+ // Return the random array to writePassword function so we can display it in the webpage as your new password! 
 return randomPW; 
 }
 
