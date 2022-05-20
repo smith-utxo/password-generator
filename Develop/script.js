05938dfb47593845937d7948644e4634cf02b8a3
Adam Smith <smith.adam937@gmail.com>
2022-05-20 10:50:34 -0500

    new branch for if logic fix

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,6 @@
 // Assignment code here
+//Create variables for different types of arrays 
+
 var lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; 
